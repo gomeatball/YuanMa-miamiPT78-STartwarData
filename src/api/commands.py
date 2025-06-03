aa2ae4c7f0774763cf8aa2a264b0e095e15d7db2
Yuan Ma <mayuan131522@gmail.com>
2025-06-03 01:24:38 +0000

    new update

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,33 +1,57 @@
 
 import click
-from api.models import db, User
+from api.models import db, User, Person, Planet
 
 """
 In this file, you can add as many commands as you want using the @app.cli.command decorator
 Flask commands are usefull to run cronjobs or tasks outside of the API but sill in integration 
 with youy database, for example: Import the price of bitcoin every night as 12am
 """
+
+
 def setup_commands(app):
-    
     """ 
     This is an example command "insert-test-users" that you can run from the command line
     by typing: $ flask insert-test-users 5
     Note: 5 is the number of users to add
     """
-    @app.cli.command("insert-test-users") # name of our command
-    @click.argument("count") # argument of out command
-    def insert_test_users(count):
+    @app.cli.command("insert-test-users")  # name of our command
+    # @click.argument("count") # argument of out command
+    def insert_test_users():
+        user_list = ['johnsmith123', 'sammysmith456', 'mustangsally789']
+
         print("Creating test users")
-        for x in range(1, int(count) + 1):
+        for x in range(0, len(user_list)):
             user = User()
-            user.email = "test_user" + str(x) + "@test.com"
-            user.password = "123456"
-            user.is_active = True
+            user.username = user_list[x]
             db.session.add(user)
             db.session.commit()
-            print("User: ", user.email, " created.")
+            print("User: ", user.username, " created.")
 
         print("All test users created")
+
+    @app.cli.command("create-people")
+    def insert_test_people():
+        person_name_list = ['Luke Skywalker', 'Darth Vader',
+                            'C-3PO', 'Leah Organa', 'Obiwan Kenobi']
+        person_hair_color = ['blonde', 'none', 'none', 'black', 'white']
+
+        print("Creating test users")
+        for x in range(0, len(person_name_list)):
+            person = Person()
+            person.name = person_name_list[x]
+            person.hair_color = person_hair_color[x]
+            db.session.add(person)
+            db.session.commit()
+            print("Person: ", person.name, " created.")
+
+        print("All test people created")
+
+    #@app.cli.command("create-planet")
+    #def insert_test_planet():
+        planet_name_list = []
+        planet_terrain_list = []
+     #   print("Creating test planet")
 
     @app.cli.command("insert-test-data")
     def insert_test_data():
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Person, Planet
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -12,11 +12,100 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/planet', methods=['POST', 'GET'])
+def get_planet():
+    all_planet = Planet.query.all()
+    if all_planet is None:
+        return jsonify("Sorry! No star wars planet found!"), 404
+    else:
+        all_planet = list(map(lambda x: x.serialize(), all_planet))
+        return jsonify(all_planet), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+
+@api.route('/planet/<int:planet_id>', methods=['GET'])
+def get_single_planet(planet_id):
+
+    single_planet = db.session.get(Planet, planet_id)
+    if single_planet is None:
+        raise APIException(
+            f"Planet ID {planet_id} was not found", status_code=404)
+
+    single_planet = single_planet.serialize()
+    return jsonify(single_planet), 200
+
+
+@api.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    pass
+
+
+@api.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def remove_favorite_planet(planet_id):
+    pass
+
+
+@api.route('/people', methods=["GET"])
+def get_people():
+    all_people = Person.query.all()
+    if all_people is None:
+        return jsonify("Sorry! No star wars characters found!"), 404
+    else:
+        all_people = list(map(lambda x: x.serialize(), all_people))
+        return jsonify(all_people), 200
+
+
+@api.route('/people/<int:person_id>', methods=['GET'])
+def get_single_person(person_id):
+
+    single_person = db.session.get(Person, person_id)
+    if single_person is None:
+        raise APIException(
+            f"Person ID {person_id} was not found", status_code=404)
+
+    single_person = single_person.serialize()
+    return jsonify(single_person), 200
+
+
+@api.route('/favorite/people/<int:person_id>', methods=['POST'])
+def add_favorite_person(person_id):
+    user = request.get_json()
+    print(user)
+    user = db.session.get(User.user["user_id"])
+    person = db.session.get(Person, person_id)
+
+    user.favorite_people.append(person)
+    db.session.commit()
+
+    return jsonify(f'User {user.username} has added {person.name} to their favorites,'), 200
+
+
+@api.route('/favorite/people/<int:person_id>', methods=['DELETE'])
+def remove_favorite_person(person_id):
+    pass
+
+
+@api.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    if all_users is None:
+        return jsonify('Sorry! No user found!'), 404
+    else:
+        all_users = list(map(lambda x: x.serialize(), all_users))
+        return jsonify(all_users), 200
+
+
+@api.route('/users/<int:user_id>/favorites', methods=['GET'])
+def get_user_favorites(user_id):
+    current_users = db.session.get(User, user_id)
+    if current_users is None:
+        return APIException('Sorry! No user found!', status_code=404)
+
+    all_people = [each_person.serialize()
+                  for each_person in current_users.favorite_people]
+
+    response = {
+        "message": f'User {current_users.username}\'s list of favorite people',
+        "data": all_people,
     }
 
-    return jsonify(response_body), 200
+    return jsonify(response), 200
